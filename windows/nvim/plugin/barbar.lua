c20839531d8cc952ab3ec408c4f2a24984cc60f7
kdayivon <kayivon001@gmail.com>
2026-06-06 22:39:03 +0200

    fix: barbar.nvim loading devicons

--- windows/nvim/plugin/barbar.lua
+++ windows/nvim/plugin/barbar.lua
@@ -1,7 +1,8 @@
 -- barbar.lua   
 vim.pack.add({
-    { src = 'https://github.com/romgrk/barbar.nvim' },
+    { src = 'https://github.com/nvim-tree/nvim-web-devicons', lazy = false },
     { src = 'https://github.com/lewis6991/gitsigns.nvim' },
+    { src = 'https://github.com/romgrk/barbar.nvim' },
 })
 
 vim.g.barbar_auto_setup = false
