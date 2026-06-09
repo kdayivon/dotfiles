50ed31859680eff4107916d754804c1935a67bf6
kdayivon <kayivon001@gmail.com>
2026-06-09 13:59:22 +0200

    add: vimlatex, change: opts

--- windows/nvim/lua/opts.lua
+++ windows/nvim/lua/opts.lua
@@ -7,7 +7,9 @@ vim.g.loaded_netrwPlugin = 1
 
 vim.opt.number = true
 vim.opt.relativenumber = true
+vim.opt.wrap = false
 vim.opt.tabstop = 4
+vim.opt.swapfile = false
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
